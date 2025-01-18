a7cf53127a51647d1d83002e81531fce6bdccc06
girishm3214 <m.girishroy@gmail.com>
2025-01-18 16:50:14 +0530

    firebase env

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -9,12 +10,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCKjY6ZM1ZxDXHHlR25J69WTDjKDYhK4B0",
+  apiKey: process.env.API_KEY,
   authDomain: "todo-list-bfd1b.firebaseapp.com",
   projectId: "todo-list-bfd1b",
   storageBucket: "todo-list-bfd1b.firebasestorage.app",
   messagingSenderId: "448549797663",
-  appId: "1:448549797663:web:e3b616a43119d4108aba2a",
+  appId: process.env.APP_ID,
   measurementId: "G-B1ML3R95HH",
 };
 
@@ -23,5 +24,8 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firestore
 const db = getFirestore(app);
 
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
 export default app;
-export { app, db };
+export { app, db, googleProvider, auth };
